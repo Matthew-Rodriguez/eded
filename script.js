ec9e36e6600bf3323f01a859cd7d773255aedab9
Matthew Rodriguez <36720171+Matthew-Rodriguez@users.noreply.github.com>
2021-10-03 22:40:08 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+
+//dropdown
+
+
+var navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+    navLinks.style.right = "0";
+}
+
+function hideMenu() {
+    navLinks.style.right = "-200px";
+}
+
+
+//contact form
+
+
+
+
+
+
+const checkbox = document.querySelector('.my-form input[type="checkbox"]');
+const btns = document.querySelectorAll(".my-form button");
+
+checkbox.addEventListener("change", function() {
+  const checked = this.checked;
+  for (const btn of btns) {
+    checked ? (btn.disabled = false) : (btn.disabled = true);
+  }
+});
